b778cccd7a429f6b02483b4f734d9cb0e0d1bacc
vhivestate <garcia.valeria001@gmail.com>
2022-06-19 18:13:26 -0600

    set up node

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'P8RX77z7',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+  
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
